1ec75cec5ba27f71898990c82672b6ec2420fbb7
masabai <winthutaye@gmail.com>
2025-09-05 19:57:53 -0700

    Merge remote-tracking branch 'RealEstate/master'

---
+++ dbt/real_estate_dbt/models/marts/sales_over_time.sql
@@ -0,0 +1,17 @@
+-- Distribution of sales over time
+{{ config(
+    materialized='table'
+) }}
+
+WITH sales_by_year AS (
+    SELECT
+        list_year,
+        COUNT(*) AS sales_count,
+        ROUND(AVG(sale_amount::NUMERIC),2) AS avg_sale,
+        ROUND(AVG(sale_amount::NUMERIC - assessed_value::NUMERIC),2) AS avg_price_difference,
+        ROUND(AVG(sale_amount::NUMERIC / NULLIF(assessed_value::NUMERIC,0)),2) AS avg_sales_ratio
+    FROM {{ ref('fact_sales') }}
+    GROUP BY list_year
+)
+SELECT *
+FROM sales_by_year

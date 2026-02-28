ae1919f36c99440c7fec0a6d14dbfdcbcfa7bb25
MAye <winthutaye@gmail.com>
2026-02-27 17:37:47 -0800

    Update comment in generate_schema_name macro
    Clarify the comment regarding schema usage.

--- dbt/real_estate_dbt/macros/generate_schema_name.sql
+++ dbt/real_estate_dbt/macros/generate_schema_name.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {#
-      Use exactly what you set in +schema: (like "staging" or "clean"),
+      Use schema: (like "staging" or "clean"),
       otherwise fall back to target.schema
     #}
     {{ custom_schema_name if custom_schema_name is not none else target.schema }}
